2b00fc29aa36987384a92b9d86c205ffb57832ed
oyevamos <deadhead2520@gmail.com>
2024-03-25 22:15:13 +0300

    commit

--- main.go
+++ main.go
@@ -2,49 +2,41 @@ package main
 
 import (
 	"fmt"
-	"github.com/go-chi/chi/v5"
-	"html/template"
-	"log"
 	"net/http"
 	"path/filepath"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/oyevamos/jon_course.git/controllers"
+	"github.com/oyevamos/jon_course.git/views"
 )
 
-func executeTemplate(w http.ResponseWriter, filepath string) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	tpl, err := template.ParseFiles(filepath)
-	if err != nil {
-		log.Printf("parsing template %v", err)
-		http.Error(w, "There was an error parsing the tamplate", http.StatusInternalServerError)
-		return
-	}
-	err = tpl.Execute(w, nil)
-	if err != nil {
-		log.Printf("executing template %v", err)
-		http.Error(w, "There was an error executing the tamplate", http.StatusInternalServerError)
-		return
-	}
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	tplPath := filepath.Join("templates", "home.gohtml")
-	executeTemplate(w, tplPath)
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	tplPath := filepath.Join("templates", "contact.gohtml")
-	executeTemplate(w, tplPath)
-}
-
-func faqHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>FAQ Page</h1><p>Is there is a free version?</p><p>Yes! We offer a free trial for 30 days on any paid plans</p>")
-}
+var (
+	homeTemplate views.Template
+)
 
 func main() {
 	r := chi.NewRouter()
-	r.Get("/", homeHandler)
-	r.Get("/contact", contactHandler)
-	r.Get("/faq", faqHandler)
+
+	tpl, err := views.Parse(filepath.Join("Templates", "home.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/", controllers.StaticHandler(tpl))
+
+	tpl, err = views.Parse(filepath.Join("templates", "contact.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+
+	r.Get("/contact", controllers.StaticHandler(tpl))
+
+	tpl, err = views.Parse(filepath.Join("templates", "faq.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+
+	r.Get("/faq", controllers.StaticHandler(tpl))
+
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
